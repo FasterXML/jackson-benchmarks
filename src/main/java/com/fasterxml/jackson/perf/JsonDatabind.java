a8a741344698a339ca6ea394fd7e5102afbf6994
Tatu Saloranta <tatu.saloranta@iki.fi>
2014-05-08 23:06:49 -0700

    Skeletal version; can't run for some reason

--- src/main/java/com/fasterxml/jackson/perf/JsonDatabind.java
+++ src/main/java/com/fasterxml/jackson/perf/JsonDatabind.java
@@ -4,12 +4,16 @@ import java.util.concurrent.TimeUnit;
 
 import org.openjdk.jmh.annotations.GenerateMicroBenchmark;
 import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.logic.BlackHole;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectReader;
 
-public class JsonDatabind extends PerfBase
+@State(Scope.Thread) // or Group or Benchmark
+@OutputTimeUnit(TimeUnit.SECONDS)
+public class JsonDatabind // extends PerfBase
 {
     private static final ObjectReader JACKSON_READER = new ObjectMapper().reader(Object.class);
 
@@ -18,19 +22,16 @@ public class JsonDatabind extends PerfBase
     }
 
     @GenerateMicroBenchmark
-    @OutputTimeUnit(TimeUnit.SECONDS)
     public void citmCatalogWS(BlackHole bh) throws Exception {
         bh.consume(process(TestFiles.CITM_CATALOG_WS.jsonBytes()));
     }
 
     @GenerateMicroBenchmark
-    @OutputTimeUnit(TimeUnit.SECONDS)
     public void webxmlWS(BlackHole bh) throws Exception {
         bh.consume(process(TestFiles.WEBXML_WS.jsonBytes()));
     }
 
     @GenerateMicroBenchmark
-    @OutputTimeUnit(TimeUnit.SECONDS)
     public void menuWS(BlackHole bh) throws Exception {
         bh.consume(process(TestFiles.MENU_WS.jsonBytes()));
     }
