aa7ab0c58cf9e42e54642319e8b746a0a9811814
Tatu <tsaloranta@gmail.com>
2014-05-22 14:15:12 -0700

    ...

--- src/main/java/com/fasterxml/jackson/perf/json/JacksonJrDatabind.java
+++ src/main/java/com/fasterxml/jackson/perf/json/JacksonJrDatabind.java
@@ -2,9 +2,7 @@ package com.fasterxml.jackson.perf.json;
 
 import java.util.concurrent.TimeUnit;
 
-import org.openjdk.jmh.annotations.OutputTimeUnit;
-import org.openjdk.jmh.annotations.State;
-import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.*;
 import org.openjdk.jmh.logic.BlackHole;
 
 import com.fasterxml.jackson.jr.ob.JSON;
@@ -13,11 +11,11 @@ import com.fasterxml.jackson.perf.PerfTestLimited;
 import com.fasterxml.jackson.perf.data.InputConverter;
 import com.fasterxml.jackson.perf.model.MediaItem;
 
-@State(Scope.Group) // Thread, Group or Benchmark
 @OutputTimeUnit(TimeUnit.SECONDS)
 public class JacksonJrDatabind
 	implements PerfTestLimited
 {
+//    @State(Scope.Group) // Thread, Group or Benchmark
     private static final JSON json = JSON.std;
     
     // null; will use JSON input as-is
@@ -25,13 +23,19 @@ public class JacksonJrDatabind
 
     public JacksonJrDatabind() { }
 
-	@Override
-	public void readPojoMediaItem(BlackHole bh) throws Exception {
+    @GenerateMicroBenchmark
+    @Override
+    public void readPojoMediaItem(BlackHole bh) throws Exception {
         bh.consume(read(NO_OP.bytesForMediaItem()));
-	}
+    }
 
-	private MediaItem read(byte[] input) throws Exception
-	{
-		return json.beanFrom(MediaItem.class, input);
-	}
+    private MediaItem read(byte[] input) throws Exception
+    {
+        try {
+            return json.beanFrom(MediaItem.class, input);
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw e;
+        }
+    }
 }
