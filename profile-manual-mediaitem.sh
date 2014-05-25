4459edc2dd7a6e0b7ec175120443e5a92c20f30d
Tatu Saloranta <tatu.saloranta@iki.fi>
2014-05-25 01:23:20 -0700

    ...

---
+++ profile-manual-mediaitem.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+java -Xmx256m -server -cp target/microbenchmarks.jar \
+ -Xrunhprof:cpu=samples,depth=10,verbose=n,interval=2 \
+ com.fasterxml.jackson.perf.manual.ManualMediaItemRead
+
