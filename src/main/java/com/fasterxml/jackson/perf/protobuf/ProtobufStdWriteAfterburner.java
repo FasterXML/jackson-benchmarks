3d3a5ff80438dbf08bb41104bfce9df4d747b9cb
Tatu Saloranta <tatu.saloranta@iki.fi>
2014-11-10 19:18:20 -0800

    Add test for jackson protobuf buffer (only writing works at this point)

---
+++ src/main/java/com/fasterxml/jackson/perf/protobuf/ProtobufStdWriteAfterburner.java
@@ -0,0 +1,26 @@
+package com.fasterxml.jackson.perf.protobuf;
+
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.Scope;
+
+import com.fasterxml.jackson.databind.*;
+import com.fasterxml.jackson.dataformat.protobuf.ProtobufFactory;
+import com.fasterxml.jackson.dataformat.protobuf.schema.ProtobufSchema;
+import com.fasterxml.jackson.module.afterburner.AfterburnerModule;
+import com.fasterxml.jackson.perf.WritePerfBasicJackson;
+
+@State(Scope.Group) // Thread, Group or Benchmark
+public class ProtobufStdWriteAfterburner
+    extends WritePerfBasicJackson
+{
+    private static final ObjectMapper MAPPER = new ObjectMapper(new ProtobufFactory());
+    static {
+        MAPPER.registerModule(new AfterburnerModule());
+    }
+
+    private final static ProtobufSchema _mediaItemSchema = ProtobufHelper.mediaItemSchema();
+
+    public ProtobufStdWriteAfterburner() {
+        super(MAPPER, _mediaItemSchema);
+    }
+}
