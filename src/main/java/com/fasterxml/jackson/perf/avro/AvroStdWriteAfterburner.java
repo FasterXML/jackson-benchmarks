2227776c1be8e5abc0430ff541ba9138a919a404
Tatu Saloranta <tatu.saloranta@iki.fi>
2015-01-20 22:10:47 -0800

    ... more cleaning up wrt Avro

--- src/main/java/com/fasterxml/jackson/perf/avro/AvroStdWriteAfterburner.java
+++ src/main/java/com/fasterxml/jackson/perf/avro/AvroStdWriteAfterburner.java
@@ -3,10 +3,8 @@ package com.fasterxml.jackson.perf.avro;
 import org.openjdk.jmh.annotations.State;
 import org.openjdk.jmh.annotations.Scope;
 
-import com.fasterxml.jackson.databind.*;
-import com.fasterxml.jackson.dataformat.avro.AvroFactory;
+import com.fasterxml.jackson.dataformat.avro.AvroMapper;
 import com.fasterxml.jackson.dataformat.avro.AvroSchema;
-import com.fasterxml.jackson.dataformat.avro.schema.AvroSchemaGenerator;
 import com.fasterxml.jackson.module.afterburner.AfterburnerModule;
 import com.fasterxml.jackson.perf.WritePerfBasicJackson;
 import com.fasterxml.jackson.perf.model.MediaItem;
@@ -15,21 +13,18 @@ import com.fasterxml.jackson.perf.model.MediaItem;
 public class AvroStdWriteAfterburner
     extends WritePerfBasicJackson<MediaItem>
 {
-    private final static AvroFactory _f = new AvroFactory();
-    private static final ObjectMapper MAPPER = new ObjectMapper(_f);
+    private static final AvroMapper MAPPER = new AvroMapper();
     static {
         MAPPER.registerModule(new AfterburnerModule());
     }
 
     private final static AvroSchema _mediaItemSchema;
     static {
-	    AvroSchemaGenerator gen = new AvroSchemaGenerator();
-	    try {
-	    	MAPPER.acceptJsonFormatVisitor(MediaItem.class, gen);
-	    } catch (Exception e) {
-	    	throw new RuntimeException(e);
-	    }
-	    _mediaItemSchema = gen.getGeneratedSchema();
+         try {
+             _mediaItemSchema = MAPPER.schemaFor(MediaItem.class);
+         } catch (Exception e) {
+             throw new RuntimeException(e);
+         }
     }
 
     public AvroStdWriteAfterburner() {
