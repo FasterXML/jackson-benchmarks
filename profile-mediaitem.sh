35151afe24582e640e7e7ac0388d2d37029660d4
Cowtowncoder <tatu.saloranta@iki.fi>
2015-01-23 17:20:09 -0800

    ...

--- profile-mediaitem.sh
+++ profile-mediaitem.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-java -server -Xrunhprof:cpu=samples,depth=10,verbose=n,interval=2  -jar target/microbenchmarks.jar ".*StdRead.*PojoMediaItem.*" -wi 4 -i 5 -f 9 -t 1
+java -server -Xrunhprof:cpu=samples,depth=10,verbose=n,interval=2  -jar target/microbenchmarks.jar ".*StdRead.*PojoMediaItem.*" -wi 8 -i 30 -f 1 -t 1
