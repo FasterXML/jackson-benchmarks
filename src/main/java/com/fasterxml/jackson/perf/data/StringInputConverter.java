0cfc8e3d7e46b3f97baf94a582bfa9d94cba21f7
Tatu Saloranta <tatu.saloranta@iki.fi>
2015-06-17 22:21:57 -0700

    start adding string-backed json reader

--- src/main/java/com/fasterxml/jackson/perf/data/StringInputConverter.java
+++ src/main/java/com/fasterxml/jackson/perf/data/StringInputConverter.java
@@ -2,44 +2,51 @@ package com.fasterxml.jackson.perf.data;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.nio.charset.Charset;
 import java.util.EnumMap;
 
 import com.fasterxml.jackson.core.*;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.perf.model.MediaItems;
 
-public class InputConverter
-	extends MinimalInputConverter
+/**
+ * Alternative to {@link InputConverter} used when input read via Reader,
+ * or directly from String.
+ */
+public class StringInputConverter
+    extends InputConverter
 {
-    protected final EnumMap<InputData, byte[]> _data;
+    protected final static Charset UTF8 = Charset.forName("UTF-8");
 
-    protected InputConverter(EnumMap<InputData, byte[]> genericData,
+    protected final EnumMap<InputData, String> _strings;
+    
+    protected StringInputConverter(EnumMap<InputData,String> genericData,
     		byte[] mib)
 	{
-        super(mib);
-        _data = genericData;
+        super(null, mib);
+        _strings = genericData;
 	}
 
-    public static InputConverter nopConverter(ObjectMapper targetMapper)
+    public static StringInputConverter nopConverter(ObjectMapper targetMapper)
     {
-        EnumMap<InputData, byte[]> data = new EnumMap<InputData, byte[]>(InputData.class);
+        EnumMap<InputData,String> data = new EnumMap<>(InputData.class);
 
         // special case, mostly just to support non-converted JSON
         for (InputData input : InputData.values()) {
-        	data.put(input, input.bytes());
+            data.put(input, new String(input.bytes(), UTF8));
         }
         // but can't avoid serializing typed values
         try {
 	        byte[] mib = targetMapper.writeValueAsBytes(MediaItems.stdMediaItem());
-	        return new InputConverter(data, mib);
+	        return new StringInputConverter(data, mib);
         } catch (IOException e) {
         	throw new RuntimeException(e);
         }
     }
 
-    public static InputConverter stdConverter(ObjectMapper targetMapper)
+    public static StringInputConverter stdConverter(ObjectMapper targetMapper)
     {
-        EnumMap<InputData, byte[]> data = new EnumMap<InputData, byte[]>(InputData.class);
+        EnumMap<InputData,String> data = new EnumMap<>(InputData.class);
         ByteArrayOutputStream bytes = new ByteArrayOutputStream(4000);
         final JsonFactory jsonF = new JsonFactory();
         final JsonFactory targetF = targetMapper.getFactory();
@@ -54,16 +61,21 @@ public class InputConverter
 	            }
 	            in.close();
 	            out.close();
-	            data.put(input, bytes.toByteArray());
+	            data.put(input, bytes.toString("UTF-8"));
 	        }
 	        byte[] mib = targetMapper.writeValueAsBytes(MediaItems.stdMediaItem());
-	        return new InputConverter(data, mib);
+	        return new StringInputConverter(data, mib);
         } catch (IOException e) {
         	throw new RuntimeException(e);
         }
     }
 
+    @Override
     public byte[] bytes(InputData type) {
-        return _data.get(type);
+        throw new UnsupportedOperationException("Should not be called for "+getClass().getName());
+    }
+
+    public String asString(InputData type) {
+        return _strings.get(type);
     }
 }
---
+++ src/main/java/com/fasterxml/jackson/perf/json/JsonStringReadVanilla.java
@@ -0,0 +1,130 @@
+package com.fasterxml.jackson.perf.json;
+
+import java.io.*;
+import java.util.concurrent.TimeUnit;
+
+import org.openjdk.jmh.annotations.GenerateMicroBenchmark;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.logic.BlackHole;
+
+import com.fasterxml.jackson.databind.*;
+import com.fasterxml.jackson.perf.*;
+import com.fasterxml.jackson.perf.data.InputData;
+import com.fasterxml.jackson.perf.data.StringInputConverter;
+import com.fasterxml.jackson.perf.model.MediaItem;
+
+/**
+ * Alternative version that reads input using {@link java.io.Reader} instead of
+ * {@link java.io.InputStream}.
+ */
+@State(Scope.Thread)
+@OutputTimeUnit(TimeUnit.SECONDS)
+public class JsonStringReadVanilla
+    implements ReadPerfTestFull
+{
+    private static final ObjectMapper MAPPER = new ObjectMapper();
+
+    // pass non-null ObjectMapper: will remove whitespace, if any
+    private final static StringInputConverter STRING_CONVERTER = StringInputConverter.stdConverter(MAPPER);
+
+    protected final ObjectReader UNTYPED_READER;
+    protected final ObjectReader NODE_READER;
+    protected final ObjectReader MEDIA_ITEM_READER;
+    
+    protected final StringInputConverter _converter;
+
+    public JsonStringReadVanilla() {
+//        super(MediaItem.class, STRING_CONVERTER, MAPPER);
+        _converter = STRING_CONVERTER;
+        UNTYPED_READER = MAPPER.reader(Object.class);
+        NODE_READER = MAPPER.reader(JsonNode.class);
+        MEDIA_ITEM_READER = MAPPER.reader(MediaItem.class);
+    }
+
+    protected Object read(String input, ObjectReader reader) throws IOException {
+        return reader.readValue(input);
+    }
+
+    /*
+    /**********************************************************************
+    /* Typed reading tests
+    /**********************************************************************
+     */
+
+    @GenerateMicroBenchmark
+    @OutputTimeUnit(TimeUnit.SECONDS)
+    @Override
+    public void readPojoMediaItem(BlackHole bh) throws Exception {
+        bh.consume(read(_converter.mediaItemAsString(), MEDIA_ITEM_READER));
+    }
+
+    /*
+    /**********************************************************************
+    /* Untyped ("map") reading tests
+    /**********************************************************************
+     */
+
+    @GenerateMicroBenchmark
+    @OutputTimeUnit(TimeUnit.SECONDS)
+    @Override
+    public void readUntypedCitmCatalog(BlackHole bh) throws Exception {
+        bh.consume(read(_converter.asString(InputData.CITM_CATALOG_WS), UNTYPED_READER));
+    }
+
+    @GenerateMicroBenchmark
+    @OutputTimeUnit(TimeUnit.SECONDS)
+    @Override
+    public void readUntypedWebxml(BlackHole bh) throws Exception {
+        bh.consume(read(_converter.asString(InputData.WEBXML_WS), UNTYPED_READER));
+    }
+
+    @GenerateMicroBenchmark
+    @OutputTimeUnit(TimeUnit.SECONDS)
+    @Override
+    public void readUntypedMenu(BlackHole bh) throws Exception {
+        bh.consume(read(_converter.asString(InputData.MENU_WS), UNTYPED_READER));
+    }
+
+    @GenerateMicroBenchmark
+    @OutputTimeUnit(TimeUnit.SECONDS)
+    @Override
+    public void readUntypedMediaItem(BlackHole bh) throws Exception {
+        bh.consume(read(_converter.mediaItemAsString(), UNTYPED_READER));
+    }
+
+    /*
+    /**********************************************************************
+    /* JsonNode reading tests
+    /**********************************************************************
+     */
+
+    @GenerateMicroBenchmark
+    @OutputTimeUnit(TimeUnit.SECONDS)
+    @Override
+    public void readNodeCitmCatalog(BlackHole bh) throws Exception {
+        bh.consume(read(_converter.asString(InputData.CITM_CATALOG_WS), NODE_READER));
+    }
+
+    @GenerateMicroBenchmark
+    @OutputTimeUnit(TimeUnit.SECONDS)
+    @Override
+    public void readNodeWebxml(BlackHole bh) throws Exception {
+        bh.consume(read(_converter.asString(InputData.WEBXML_WS), NODE_READER));
+    }
+
+    @GenerateMicroBenchmark
+    @OutputTimeUnit(TimeUnit.SECONDS)
+    @Override
+    public void readNodeMenu(BlackHole bh) throws Exception {
+        bh.consume(read(_converter.asString(InputData.MENU_WS), NODE_READER));
+    }
+
+    @GenerateMicroBenchmark
+    @OutputTimeUnit(TimeUnit.SECONDS)
+    @Override
+    public void readNodeMediaItem(BlackHole bh) throws Exception {
+        bh.consume(read(_converter.mediaItemAsString(), NODE_READER));
+    }
+}
