c3e49f177707fe2c59d05095676e0a51918ef890
Tatu Saloranta <tatu.saloranta@iki.fi>
2015-09-27 22:35:22 -0700

    minor result reshuffling

--- profile-mediaitem.sh
+++ profile-mediaitem.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-java -server -Xrunhprof:cpu=samples,depth=10,verbose=n,interval=2  -jar target/microbenchmarks.jar ".*StdRead.*PojoMediaItem.*" -wi 8 -i 30 -f 1 -t 1
+java -server -Xrunhprof:cpu=samples,depth=10,verbose=n,interval=2  -jar target/microbenchmarks.jar ".*StdRead.*PojoMediaItem.*" -wi 5 -w 2 -i 10 -r 5 -f 1 -t 1
