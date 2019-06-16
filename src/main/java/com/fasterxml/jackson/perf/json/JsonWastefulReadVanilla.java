1f78886c42de4e7f44de4500f969c9f74b7cd5f1
Tatu Saloranta <tatu.saloranta@iki.fi>
2019-06-15 21:09:54 -0700

    Add 2 more "wasteful" read tests for JsonNode, Map/Object

--- src/main/java/com/fasterxml/jackson/perf/json/JsonWastefulReadVanilla.java
+++ src/main/java/com/fasterxml/jackson/perf/json/JsonWastefulReadVanilla.java
@@ -1,7 +1,9 @@
 package com.fasterxml.jackson.perf.json;
 
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
+import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.OutputTimeUnit;
 import org.openjdk.jmh.annotations.State;
 import org.openjdk.jmh.annotations.Scope;
@@ -35,7 +37,27 @@ public class JsonWastefulReadVanilla
         bh.consume(read(MINIMAL_CONV.mediaItemAsBytes()));
     }
 
+    @Benchmark
+    @OutputTimeUnit(TimeUnit.SECONDS)
+    public void readNodeMediaItem(Blackhole bh) throws Exception {
+        bh.consume(readTree(JSON_CONV.mediaItemAsBytes()));
+    }
+    
+    @Benchmark
+    @OutputTimeUnit(TimeUnit.SECONDS)
+    public void readUntypedMediaItem(Blackhole bh) throws Exception {
+        bh.consume(readUntyped(JSON_CONV.mediaItemAsBytes()));
+    }
+
     protected Object read(byte[] data) throws Exception {
         return new ObjectMapper().readValue(data, MediaItem.class);
+    }
+
+    protected JsonNode readTree(byte[] data) throws Exception {
+        return new ObjectMapper().readTree(data);
+    }
+
+    protected Object readUntyped(byte[] data) throws Exception {
+        return new ObjectMapper().readValue(data, Map.class);
     }
 }
