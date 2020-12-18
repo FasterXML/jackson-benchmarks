8711b4975b818a2015339f341a0440c141299436
Tatu Saloranta <tatu.saloranta@iki.fi>
2020-12-17 17:33:27 -0800

    ...

--- run-mediaitem.sh
+++ run-mediaitem.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-java -Xmx256m -jar target/microbenchmarks.jar ".*StdRead.*PojoMediaItem.*" -wi 4 -w 1 -i 5 -f 9 -t 1
+java -Xmx256m -jar target/microbenchmarks.jar ".*StdRead.*PojoMediaItem.*" -wi 4 -w 1 -i 5 -r 1 -f 9 -t 1
