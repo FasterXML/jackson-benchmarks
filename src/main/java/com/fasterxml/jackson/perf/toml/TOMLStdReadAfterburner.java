57a9aa0c7a6d8862d93899ea525c1758b206d733
Tatu Saloranta <tatu.saloranta@iki.fi>
2021-04-09 18:39:25 -0700

    Add TOML backend

--- src/main/java/com/fasterxml/jackson/perf/toml/TOMLStdReadAfterburner.java
+++ src/main/java/com/fasterxml/jackson/perf/toml/TOMLStdReadAfterburner.java
@@ -1,25 +1,25 @@
-package com.fasterxml.jackson.perf.yaml;
+package com.fasterxml.jackson.perf.toml;
 
 import org.openjdk.jmh.annotations.State;
 import org.openjdk.jmh.annotations.Scope;
 
 import com.fasterxml.jackson.databind.*;
-import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
+import com.fasterxml.jackson.dataformat.toml.TomlMapper;
 import com.fasterxml.jackson.module.afterburner.AfterburnerModule;
 import com.fasterxml.jackson.perf.ReadPerfBaseBasicJackson;
 import com.fasterxml.jackson.perf.data.InputConverter;
 import com.fasterxml.jackson.perf.model.MediaItem;
 
 @State(Scope.Thread)
-public class YAMLStdReadAfterburner
+public class TOMLStdReadAfterburner
 // could be full, but let's avoid since extra results not very useful
     extends ReadPerfBaseBasicJackson<MediaItem>
 {
-    private static final ObjectMapper MAPPER = YAMLMapper.builder()
+    private static final ObjectMapper MAPPER = TomlMapper.builder()
             .addModule(new AfterburnerModule())
             .build();
 
-    public YAMLStdReadAfterburner() {
+    public TOMLStdReadAfterburner() {
         super(MediaItem.class, InputConverter.stdConverter(MAPPER), MAPPER);
     }
 }
