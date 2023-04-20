af63d784e4a61d1d4d3b324767d88cd062733d31
Tatu Saloranta <tatu.saloranta@iki.fi>
2023-04-19 20:09:49 -0700

    Add CurrencyWrite test

--- src/main/java/com/fasterxml/jackson/perf/WritePerfTestFull.java
+++ src/main/java/com/fasterxml/jackson/perf/WritePerfTestFull.java
@@ -7,4 +7,7 @@ public interface WritePerfTestFull extends WritePerfTestBasic
     public void writeUntypedMediaItem(Blackhole bh) throws Exception;
 
     public void writeNodeMediaItem(Blackhole bh) throws Exception;
+
+    public void writeCurrencyPojoDefault(Blackhole bh) throws Exception;
+    public void writeCurrencyPojoFast(Blackhole bh) throws Exception;
 }
