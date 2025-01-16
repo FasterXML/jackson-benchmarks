7b71a3cc0ef6a2b04a8a792e2337590e7f73f5bb
Tatu Saloranta <tatu.saloranta@iki.fi>
2025-01-15 18:52:17 -0800

    ...

--- run-json-vanilla-read.sh
+++ run-json-vanilla-read.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-java -Xmx256m -jar target/perf.jar ".*JsonStdReadVan.*PojoMedia.*" -wi 1 -w 1 -i 999 -r 1 -f 1 -t 1
+java -Xmx256m -jar target/perf.jar ".*JsonStdReadVan.*PojoMedia.*" -wi 1 -w 1 -i 999 -r 1 -f 1 $*
 
