78949597f488f3ef3a8e4c8599cb4f5b9a97c5b7
Tatu Saloranta <tatu.saloranta@iki.fi>
2025-10-06 19:07:28 -0700

    Complete backport of manual read perf tests from 3.x

---
+++ src/main/java/com/fasterxml/jackson/perf/manual/ProtobufManualReadVanilla.java
@@ -0,0 +1,30 @@
+package com.fasterxml.jackson.perf.manual;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.State;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.protobuf.ProtobufMapper;
+import com.fasterxml.jackson.dataformat.protobuf.schema.ProtobufSchema;
+
+import com.fasterxml.jackson.perf.data.MinimalInputConverter;
+import com.fasterxml.jackson.perf.protob.ProtobufHelper;
+
+@State(Scope.Thread)
+@OutputTimeUnit(TimeUnit.SECONDS)
+public class ProtobufManualReadVanilla
+    extends ManualReadPerfTest
+{
+    private static final ObjectMapper MAPPER = new ProtobufMapper();
+
+    private final static ProtobufSchema _mediaItemSchema = ProtobufHelper.mediaItemSchema();
+
+    private final static MinimalInputConverter CONV = MinimalInputConverter.minimalConverter(MAPPER, _mediaItemSchema);
+
+    public ProtobufManualReadVanilla() {
+        super(CONV, MAPPER.reader(_mediaItemSchema));
+    }
+}
